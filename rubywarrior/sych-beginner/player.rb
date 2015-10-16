ac5fab9f42d8f864477790647b2b07c6e9daba55
RomanSokhan <romans@softwareplanet.uk.com>
2015-10-16 17:40:34 +0300

    extract environment logic

--- rubywarrior/sych-beginner/player.rb
+++ rubywarrior/sych-beginner/player.rb
@@ -1,3 +1,73 @@
+class Env
+
+  def on_all_rescued_at_dead_end(&block)
+    @on_all_rescued_at_dead_end = block
+  end
+
+  def danger(spaces, current_direction)
+    @spaces = spaces
+    @current_direction = current_direction
+    discover
+
+    @spaces.each_with_index do |space, index|
+      return false if space.captive?
+      return index + 1 if space.enemy?
+    end
+    false
+  end
+
+  def all_captives_rescued?
+    @all_captives_rescued
+  end
+
+  private
+
+  def discover
+    left_wall_discovered? unless @left_wall_discovered
+    right_wall_discovered? unless @right_wall_discovered
+    everything_discovered? unless @everything_discovered
+    check_all_captives_rescued unless @all_captives_rescued
+  end
+
+  def left_wall_discovered?
+    if @current_direction == :backward
+      @left_wall_discovered = wall_present?
+    end
+  end
+
+  def right_wall_discovered?
+    if @current_direction == :forward
+      @right_wall_discovered = wall_present?
+    end
+  end
+
+  def wall_present?
+    @spaces.find { |space| space.wall? }
+  end
+
+  def captive_present?
+    @spaces.find { |space| space.captive? }
+  end
+
+  def everything_discovered?
+    @everything_discovered = @left_wall_discovered && @right_wall_discovered
+  end
+
+  def no_captives_left?
+    !captive_present?
+  end
+
+  def check_all_captives_rescued
+    @all_captives_rescued = @everything_discovered && no_captives_left?
+    if @all_captives_rescued
+      if !@spaces.find{ |space| space.stairs? }
+        @on_all_rescued_at_dead_end.call
+      end
+    end
+  end
+
+end
+
 class Player
 
   class EndTurn < StandardError; end
@@ -6,21 +76,19 @@ class Player
   VERY_LOW_HP = 10
   ENOUGH_HEALTH = 10
 
+  def initialize
+    super
+    @env = Env.new
+    @env.on_all_rescued_at_dead_end { change_direction }
+  end
+
   def play_turn(warrior)
     @warrior = warrior
 
     @current_direction ||= :backward
     @prev_health ||= warrior.health
 
-    begin
-      tactical_retreat
-      look
-      change_direction_if_needed!
-      look
-      rescue_captive!
-      go!
-    rescue EndTurn
-    end
+    perform_action
 
     @prev_health = warrior.health
   end
@@ -29,55 +97,22 @@ class Player
 
   attr_reader :warrior, :prev_health, :current_direction
 
+  def perform_action
+    tactical_retreat
+    look
+    change_direction_if_needed!
+    look
+    rescue_captive!
+    go!
+  rescue EndTurn
+  end
+
   def look
     if enemy_disatance = danger(current_direction)
       shoot! if enemy_disatance <= SHOOT_DISTANCE
     end
   end
 
-  def discover(env)
-    left_wall_discovered?(env) unless @left_wall_discovered
-    right_wall_discovered?(env) unless @right_wall_discovered
-    everything_discovered? unless @everything_discovered
-    all_captives_rescued?(env) unless @all_captives_rescued
-  end
-
-  def left_wall_discovered?(env)
-    if current_direction == :backward
-      @left_wall_discovered = wall_present?(env)
-    end
-  end
-
-  def right_wall_discovered?(env)
-    if current_direction == :forward
-      @right_wall_discovered = wall_present?(env)
-    end
-  end
-
-  def wall_present?(env)
-    env.find { |space| space.wall? }
-  end
-
-  def captive_present?(env)
-    env.find { |space| space.captive? }
-  end
-
-  def everything_discovered?
-    @everything_discovered = @left_wall_discovered && @right_wall_discovered
-  end
-
-  def no_captives_left?(env)
-    !captive_present?(env)
-  end
-
-  def all_captives_rescued?(env)
-    @all_captives_rescued = @everything_discovered && no_captives_left?(env)
-    if @all_captives_rescued
-      if !env.find{ |space| space.stairs? }
-        change_direction
-      end
-    end
-  end
 
   def shoot!
     warrior.shoot!(current_direction) and raise EndTurn
@@ -92,15 +127,8 @@ class Player
   end
 
   def danger(direction)
-    env = warrior.look(direction)
-
-    discover(env)
-
-    env.each_with_index do |space, index|
-      return false if space.captive?
-      return index + 1 if space.enemy?
-    end
-    false
+    spaces = warrior.look(direction)
+    @env.danger(spaces, current_direction)
   end
 
   def stop_retreat
@@ -165,7 +193,7 @@ class Player
     if feel.wall?
       change_direction
     elsif feel.stairs?
-      if !@all_captives_rescued
+      if !@env.all_captives_rescued?
         change_direction
       end
     end
