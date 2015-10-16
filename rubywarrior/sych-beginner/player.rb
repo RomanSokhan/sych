86e7e9579a7d681ae0f2e8caa545d145d76cc440
RomanSokhan <romans@softwareplanet.uk.com>
2015-10-16 14:22:11 +0300

    Level 4-5

--- rubywarrior/sych-beginner/player.rb
+++ rubywarrior/sych-beginner/player.rb
@@ -1,21 +1,43 @@
 class Player
 
-  attr_reader :warrior
+  class EndTurn < StandardError; end
 
-  def rest_if_needed!
-    return if warrior.health > 15
-    warrior.rest!
-    true
-  end
+  attr_reader :warrior, :prev_health
 
   def play_turn(warrior)
+    @prev_health ||= warrior.health
     @warrior = warrior
 
+    begin
+      rescue_captive!
+      kill_em_all!
+    rescue EndTurn
+    end
+
+    @prev_health = warrior.health
+  end
+
+  def kill_em_all!
     if warrior.feel.empty?
-      return if rest_if_needed!
+      rest_if_needed!
       warrior.walk!
     else
       warrior.attack!
+    end
+  end
+
+  def under_attack?
+    prev_health > warrior.health
+  end
+
+  def rest_if_needed!
+    return if (warrior.health > 15 || under_attack?)
+    warrior.rest! and raise EndTurn
+  end
+
+  def rescue_captive!
+    if warrior.feel.captive?
+      warrior.rescue! and raise EndTurn
     end
   end
 
