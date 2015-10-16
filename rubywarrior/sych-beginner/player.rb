ca29473ef8b99495ca8dd2f4c7d7e63e96b64b83
RomanSokhan <romans@softwareplanet.uk.com>
2015-10-16 14:35:27 +0300

    Level 6. Version 1

--- rubywarrior/sych-beginner/player.rb
+++ rubywarrior/sych-beginner/player.rb
@@ -2,13 +2,16 @@ class Player
 
   class EndTurn < StandardError; end
 
-  attr_reader :warrior, :prev_health
+  attr_reader :warrior, :prev_health, :current_direction
 
   def play_turn(warrior)
-    @prev_health ||= warrior.health
     @warrior = warrior
 
+    @current_direction = :backward
+    @prev_health ||= warrior.health
+
     begin
+      change_direction_if_needed!
       rescue_captive!
       kill_em_all!
     rescue EndTurn
@@ -17,12 +20,38 @@ class Player
     @prev_health = warrior.health
   end
 
+  def walk!
+    warrior.walk!(current_direction) and raise EndTurn
+  end
+
+  def attack!
+    warrior.attack!(current_direction) and raise EndTurn
+  end
+
+  def rescue!
+    warrior.rescue!(current_direction) and raise EndTurn
+  end
+
+  def rest!
+    warrior.rest! and raise EndTurn
+  end
+
+  def change_direction_if_needed!
+    if warrior.feel(current_direction).wall?
+      if current_direction == :backward
+        @current_direction == :forward
+      else
+        @current_direction == :backward
+      end
+    end
+  end
+
   def kill_em_all!
     if warrior.feel.empty?
       rest_if_needed!
-      warrior.walk!
+      walk!
     else
-      warrior.attack!
+      attack!
     end
   end
 
@@ -32,12 +61,12 @@ class Player
 
   def rest_if_needed!
     return if (warrior.health > 15 || under_attack?)
-    warrior.rest! and raise EndTurn
+    rest!
   end
 
   def rescue_captive!
     if warrior.feel.captive?
-      warrior.rescue! and raise EndTurn
+      rescue!
     end
   end
 
